52f6e496c5f737cf3b4903b8856429eaca25d34e
shubhambathla <shubhambathla116@gmail.com>
2024-06-11 23:49:49 +0530

    add end-points

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+const { todo } = require("./db");
 const { createTodo, updateTodo } = require("./types");
 
 app.use(express.json());
 
-app.post("/post", (req, res) => {
+app.post("/post", async (req, res) => {
   const todoBody = req.body;
   const createTodoCheck = createTodo.safeParse(todoBody);
 
@@ -15,11 +16,26 @@ app.post("/post", (req, res) => {
     });
     return;
   }
+
+  await todo.create({
+    title: todoBody.title,
+    description: todoBody.description,
+    completed: false,
+  });
+
+  res.json({
+    message: "New Todo Successfully Created",
+  });
 });
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", async (req, res) => {
+  const todos = await todo.find({});
+  res.json({
+    todos,
+  });
+});
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
 
@@ -28,4 +44,19 @@ app.put("/completed", (req, res) => {
       message: "Something went wrong",
     });
   }
+
+  await todo.updateOne(
+    {
+      _id: updatePayload.id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  res.json({
+    message: "todo marked as completed",
+  });
 });
+
+app.listen(3000);
